4ca47edde11341c5cc66a783756d40871da4853d
Kevin Giovanni Sánchez Robalino <2cv36985@gmail.com>
2026-03-14 17:59:42 -0500

    Refactorizo y ordeno escenas

--- scenes/dialogo.gd
+++ scenes/dialogo.gd
@@ -3,26 +3,26 @@ extends Area2D
 var player_in_range = false
 
 func _ready():
-	$mensaje.visible = false
+	$message.visible = false
 	connect("body_entered", Callable(self, "_on_body_entered"))
 	connect("body_exited", Callable(self, "_on_body_exited"))
 
 func _on_body_entered(body):
 	if body.name == "Player":
 		player_in_range = true
-		$mensaje.visible = true
-		$"../../burbuja".visible = true
+		$message.visible = true
+		$"../../Speech bubble".visible = true
 
 func _on_body_exited(body):
 	if body.name == "Player":
 		player_in_range = false
-		$mensaje.visible = false
-		$"../../burbuja".visible = false
+		$message.visible = false
+		$"../../Speech bubble".visible = false
 
 func _process(delta):
 	if player_in_range and Input.is_action_just_pressed("ui_accept"):
 		mostrar_dialogo()
 
 func mostrar_dialogo():
-	$confirmar.visible = true
+	$confirm.visible = true
 	print("¡Hola! Este es un diálogo.")
