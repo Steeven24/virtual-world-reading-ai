0234dd0676a20b2eb0ba2dcc3a981de3ae58b135
Kevin Giovanni Sánchez Robalino <2cv36985@gmail.com>
2026-03-18 11:21:09 -0500

    agrego interacción de teletransporte al confirmar la interacción

--- scripts/pressE_teleport_dialogue.gd
+++ scripts/pressE_teleport_dialogue.gd
@@ -1,11 +1,13 @@
 extends Area2D
 
+@export var next_scene_path: String
 var player_in_range = false
 
 func _ready():
 	$message.visible = false
 	connect("body_entered", Callable(self, "_on_body_entered"))
 	connect("body_exited", Callable(self, "_on_body_exited"))
+	$confirm.confirmed.connect(_on_dialog_confirmed)
 
 func _on_body_entered(body):
 	if body.name == "Player":
@@ -21,8 +23,23 @@ func _on_body_exited(body):
 
 func _process(delta):
 	if player_in_range and Input.is_action_just_pressed("ui_accept"):
-		mostrar_dialogo()
+		show_dialogue()
+			
 
-func mostrar_dialogo():
+func show_dialogue():
 	$confirm.visible = true
 	print("¡Hola! Este es un diálogo.")
+
+func change_scene():
+	get_tree().change_scene_to_file(next_scene_path)
+
+func _on_dialog_confirmed():
+	if next_scene_path != "":
+		get_tree().change_scene_to_file(next_scene_path)
+	else:
+		print("Error: No has asignado una ruta de escena en el inspector.")
+		
+		
+		
+		
+		
